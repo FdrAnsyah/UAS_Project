77df7567b9b85fe2b6ec0bde4d533e97eeee9be0
Ferdiansyah <ferdiansyah030506@gmail.com>
2025-01-19 00:44:18 +0700

    Perubahan dan update

---
+++ frontend/src/main.js
@@ -0,0 +1,41 @@
+const burgerMenu = document.querySelector('#burgerMenu');
+const navbarMenu = document.querySelector('#navbarMenu');
+const navbarLogin = document.querySelector('#navbarLogin');
+
+burgerMenu.addEventListener('click', function () {
+  burgerMenu.classList.toggle('burger-active');
+  navbarMenu.classList.toggle('hidden');
+  navbarLogin.classList.toggle('hidden');
+});
+
+const image = document.querySelector('.image');
+
+let MulaiImage = false,
+  ImagePageX,
+  ImageScrollKiri;
+
+image.addEventListener(
+  'mousedown',
+  (ImageDown = (evt) => {
+    MulaiImage = true;
+    ImagePageX = evt.pageX;
+    ImageScrollKiri = image.scrollLeft;
+  })
+);
+
+image.addEventListener(
+  'mousemove',
+  (imageMove = (evt) => {
+    if (!MulaiImage) return;
+    evt.preventDefault();
+    let posisi = evt.pageX - ImagePageX;
+    image.scrollLeft = ImageScrollKiri - posisi;
+  })
+);
+
+image.addEventListener(
+  'mouseup',
+  (ImageUp = () => {
+    MulaiImage = false;
+  })
+);
